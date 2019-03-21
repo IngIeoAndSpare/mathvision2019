9730f456c42986c050c7eb82880a35ec1a6b587e
IngIeoAndSpare <wjh4877@naver.com>
2019-03-21 14:45:05 +0900

    add vector sub method, make cos, sin value function

--- HW5/HW5.js
+++ HW5/HW5.js
@@ -1,3 +1,5 @@
+const mathModule = require('mathjs');
+
 const POW_NUM = 2;
 const X = 0,
       Y = 1,
@@ -17,13 +19,13 @@ var originPoint = [
         [0,0,0]
     ];
 
-var Vector = (x, y, z) => {
+function Vector (x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
 }
 
-var Point = (x, y, z) => {
+function Point (x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;    
@@ -34,16 +36,52 @@ module.exports = {
         let originPointArray = this.getPointArray(originPoint);
         let movePointArray = this.getPointArray(movePoint);
 
+        // R1, R2를 먼저 값을 구한 후 P4에 적용하여 P4' 가 나오는지 확인.
+        // ref : https://sjwd.tistory.com/34
+        // get vector
+        let oriFrontVector = originPointArray[0].getVector(originPointArray[1]),
+            oriBackVector = originPointArray[0].getVector(originPointArray[2]);
+
+        let moveFrontVector = movePointArray[0].getVector(movePointArray[1]),
+            moveBackVector = movePointArray[0].getVector(movePointArray[2]);
+
+        // get normal vector
+        let oriNormalVector = oriFrontVector.getCrossProduct(oriBackVector),
+            moveNormalVector = moveFrontVector.getCrossProduct(moveBackVector);
+
+        // get rotation value ref : https://darkpgmr.tistory.com/81
+        let r1CosValue = this.getCosValue(oriNormalVector, moveNormalVector);
+        let r1SinValue = this.getSinValue(r1CosValue);
+
     },
-    getPointArray : function (pointArray) {
+    getPointArray : function (sourcePointArray) {
         let pointArray = [];
-        for (let pointItem of pointArray) {
+        for (let pointItem of sourcePointArray) {
             pointArray.push(
                 new Point(pointItem[X], pointItem[Y], pointItem[Z])
             );
         }
         return pointArray;
+    },
+    /**
+     * get cos value
+     * @param {Vector} sourceVector 
+     * @param {Vector} targetVector 
+     */
+    getCosValue : function (sourceVector, targetVector) {
+        return (
+            sourceVector.getDotProduct(targetVector) /
+            sourceVector.getVectorLength() * targetVector.getVectorLength()
+        )
+    },
+    /**
+     * get sin value
+     * @param {float} cosValue 
+     */
+    getSinValue : function (cosValue) {
+        return Math.sqrt(1 - Math.pow(cosValue, POW_NUM));
     }
+
     
 }
 
@@ -74,17 +112,17 @@ Point.prototype = {
 
 Vector.prototype = {
     /**
-     * get this -> target Normal vector
-     * @param {Vector} targetVector  target vector. 
+     * get vector length. 
      */
-    getNormalVector : function (targetVector) {
-        
-    },
     getVectorLength : function () {
         return Math.sqrt(
             Math.pow(this.x, POW_NUM) + Math.pow(this.y, POW_NUM) + Math.pow(this.z, POW_NUM)
         );
     },
+    /**
+     * get Cross product. thisVector X targetVector
+     * @param {Vector} targetVector 
+     */
     getCrossProduct : function (targetVector) {
         let x = (this.y * targetVector.z) - (this.z * targetVector.y),
             y = (this.z * targetVector.x) - (this.x * targetVector.z),
@@ -92,7 +130,27 @@ Vector.prototype = {
 
         return new Vector(x, y, z);
     },
+    /**
+     * get Dot product value. thisVector * targetVector
+     * @param {Vector} targetVector 
+     */
     getDotProduct : function (targetVector) {
         return (this.x * targetVector.x) + (this.y * targetVector.y) + (this.z * targetVector.z); 
+    },
+    /**
+     * print vector x, y, z info
+     */
+    printVectorInfo : function () {
+        console.log("x => " + this.x + "   y => " + this.y + "    z => " + this.z);
+    },
+    /**
+     * convert vector to metrix
+     * @param {Flag} flag  true : return column metrix  // false : return row metrix
+     */
+    convertVectorToArray : function (flag) {
+        return (
+            flag ? [this.x, this.y, this.z] : 
+                    [[this.x, this.y, this.z]]
+        );
     }
 }
